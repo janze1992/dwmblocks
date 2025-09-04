afa27b590073cab882de5825cbdf75ab8abeddf4
janze1992 <125573413+janze1992@users.noreply.github.com>
2025-09-04 22:21:18 +0200

    config.h

--- config.h
+++ config.h
@@ -2,11 +2,30 @@
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 
-	{"",	"dwmblocks_disks", 10, 14},
-	{"",	"dwmblocks_systemstats", 10, 14},
-	{"",	"dwmblocks_musicplaying",	0,		11},
-	{"",	"dwmblocks_weather",	1800,		1},
-	{"",	"dwmblocks_timedate",	60,		1},
+	/* {"⌨", "sb-kbselect", 0, 30}, */
+	{"", "cat /tmp/recordingicon 2>/dev/null",	0,	9},
+	{"",	"sb-tasks",	10,	26},
+	/* {"",	"sb-music",	0,	11}, */
+	{"",	"sb-pacpackages",	0,	8},
+	{"",	"sb-news",		0,	6},
+	/* {"",	"sb-price xmr-btc \"Monero to Bitcoin\" 🔒 25",	9000,	25}, */
+	/* {"",	"sb-price xmr Monero 🔒 24",			9000,	24}, */
+	/* {"",	"sb-price eth Ethereum 🍸 23",			9000,	23}, */
+	/* {"",	"sb-price btc Bitcoin 💰 21",			9000,	21}, */
+	{"",	"sb-torrent",	20,	7},
+	/* {"",	"sb-memory",	10,	14}, */
+	/* {"",	"sb-cpu",		10,	18}, */
+	/* {"",	"sb-moonphase",	18000,	17}, */
+	{"",	"sb-doppler",	0,	13},
+	{"",	"sb-forecast",	18000,	5},
+	{"",	"sb-mailbox",	180,	12},
+	{"",	"sb-nettraf",	1,	16},
+	{"",	"sb-volume",	0,	10},
+	{"",	"sb-battery",	5,	3},
+	{"",	"sb-clock",	60,	1},
+	{"",	"sb-internet",	5,	4},
+	/* {"",	"sb-iplocate", 0,	27}, */
+	{"",	"sb-help-icon",	0,	15},
 };
 
 //Sets delimiter between status commands. NULL character ('\0') means no delimiter.
